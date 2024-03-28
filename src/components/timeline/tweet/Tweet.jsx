caa6afc76857eca0072e4afe0fc2a22078f43880
firmin nagnduli <finganduli@gmail.com>
2024-03-28 20:51:15 +0200

    remove reverse() on tweets

--- src/components/timeline/tweet/Tweet.jsx
+++ src/components/timeline/tweet/Tweet.jsx
@@ -4,7 +4,6 @@ import TweetContent from "./TweetContent"
 
 export default function Tweet({value}){
     return (<div className="tweet">
-
         <Link to={`/userProfile/${value["tweet_body"]["tweet_author_title"]}`}><TweetAvatar image={value["tweet_avatar"]}/></Link>
         <TweetContent value= {value.tweet_body} />
     </div>)

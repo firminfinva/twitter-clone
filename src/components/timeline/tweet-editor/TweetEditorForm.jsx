b8ec268ce9d19236c98f9a228cc86a5eba6b7539
firmin nagnduli <finganduli@gmail.com>
2024-04-18 13:37:40 +0200

    use json-server hosted on render

--- src/components/timeline/tweet-editor/TweetEditorForm.jsx
+++ src/components/timeline/tweet-editor/TweetEditorForm.jsx
@@ -1,55 +1,58 @@
-import TweetEditorInput from "./TweetEditorInput"
-import TweetEditorButtons from "./TweetEditButtons"
-import { tweetEditorForm } from "../../../tailwindClasses"
-import { useForm } from "react-hook-form"
-import { useContext } from "react"
-import TweetsContext from "../../../contexts/TweetsContext"
-import axios from "axios"
-import Button from "./Button"
+import TweetEditorInput from "./TweetEditorInput";
+import TweetEditorButtons from "./TweetEditButtons";
+import { tweetEditorForm } from "../../../tailwindClasses";
+import { useForm } from "react-hook-form";
+import { useContext } from "react";
+import TweetsContext from "../../../contexts/TweetsContext";
+import axios from "axios";
+import Button from "./Button";
 
-export default function TweetEditorForm(){
-    let {tweets, setTweets, createTweetsText,user} = useContext(TweetsContext)
-    const { register, handleSubmit, formState: {errors}} = useForm()
-    function onSubmit(p){
-        
-        let newTweet = {
-        "id": "0" + (tweets.length +1),
-        "tweet_avatar": user["tweet_avatar"],
-        "tweet_body": {
-          "tweet_author_title": user.titleAuthor,
-          "tweet_authors": user.titleAuthor2,
-          "tweet_time": "12m",
-          "tweet_actions":{
-              "reply":"0",
-              "retweet":"0",
-              "liked": "false",
-              "react":"0",
-              "share":""},
-          "tweet_text": p.tweet
-          }
-       }
+export default function TweetEditorForm() {
+  let { tweets, user, url } = useContext(TweetsContext);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  function onSubmit(p) {
+    let newTweet = {
+      id: "0" + (tweets.length + 1),
+      tweet_avatar: user["tweet_avatar"],
+      tweet_body: {
+        tweet_author_title: user.titleAuthor,
+        tweet_authors: user.titleAuthor2,
+        tweet_time: "12m",
+        tweet_actions: {
+          reply: "0",
+          retweet: "0",
+          liked: "false",
+          react: "0",
+          share: "",
+        },
+        tweet_text: p.tweet,
+      },
+    };
 
-      axios.post('http://localhost:3001/tweets', newTweet)
-      .then(response => {
-        console.log('New post added:', response.data);
+    axios
+      .post(`${url}/tweets`, newTweet)
+      .then((response) => {
+        console.log("New post added:", response.data);
       })
-      .catch(error => {
-        console.error('Error adding post:', error);
+      .catch((error) => {
+        console.error("Error adding post:", error);
       });
-    }
+  }
 
-    return (
+  return (
     <div className={tweetEditorForm}>
-        <form onSubmit={handleSubmit((data) => onSubmit(data))}>
-            <TweetEditorInput register={register} />
-            <div className="text-red-400">{errors?.tweet?.message}</div>
-            <div className="flex justify-between">
-                <TweetEditorButtons />
-                <Button type="submit" /> 
-            </div>
-            
-        </form>
-        
+      <form onSubmit={handleSubmit((data) => onSubmit(data))}>
+        <TweetEditorInput register={register} />
+        <div className="text-red-400">{errors?.tweet?.message}</div>
+        <div className="flex justify-between">
+          <TweetEditorButtons />
+          <Button type="submit" />
+        </div>
+      </form>
     </div>
-    )
+  );
 }
